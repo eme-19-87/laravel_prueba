bfc6972c41a2a40402428ed1206e0c27c8cdc954
Rodrigo <gatopunk99@gmail.com>
2023-07-12 02:04:27 -0300

    Database

---
+++ database/migrations/2023_07_12_040805_create_vaccines_table.php
@@ -0,0 +1,31 @@
+<?php
+// Tabla Intermedia Necesaria Pets
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vaccines', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vaccines');
+    }
+};
