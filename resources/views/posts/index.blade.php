fc73fe6b9682fe06cf4616d791972503d54b44ca
joaccco <joaccofranc02@icloud.com>
2023-10-30 00:48:51 -0300

    Cambios en vista Posts

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,7 +2,7 @@
 <section class="bg-white">
 
   <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16">
-    <h1 class="text-black text-3xl font-extrabold mb-10">Posts y Anuncios</h1>
+    <h1 class="text-black text-3xl font-extrabold mt-10 mb-10">Posts y Anuncios</h1>
     <div class="grid md:grid-cols-2 gap-8">
     @foreach ($posts as $post)
         {{-- @dd($post->image) --}}
@@ -16,7 +16,7 @@
         @endphp
         @if($post->image)
             <article style="background-image: url('{{ url(Storage::url($post->image->url)) }}')" class="bg-black/20 border border-gray-200 rounded-lg p-8 md:p-12 @if($loop->first) col-span-2 mb-8 @endif">
-
+                
                 @else
                 <article class="bg-[url(https://images.unsplash.com/photo-1552832230-c0197dd311b5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1592&q=80)] bg-black/20 border border-gray-200 rounded-lg p-8 md:p-12 @if($loop->first) col-span-2 mb-8 @endif">
                 @endif
@@ -26,9 +26,9 @@
                     </svg>
                     {{$post->category->name}}
                 </a>
-                <h2 class="text-black text-2xl font-extrabold mb-2">{{$post->title}}</h2>
-                <p class="text-lg font-normal text-black mb-4">{{$post->extract}}</p>
-                <a href="/posts/{{$post->id}}" class="inline-flex justify-center items-center py-2.5 px-5 text-base font-medium text-center text-black rounded-lg bg-white hover:bg-blue-400 hover:text-white focus:ring-4 focus:ring-blue-200">
+                <h2 class="text-white text-2xl font-extrabold mb-2">{{$post->title}}</h2>
+                <p class="text-lg font-normal text-white mb-4">{{$post->extract}}</p>
+                <a href="/posts/{{$post->id}}" class="inline-flex justify-center items-center py-2.5 px-5 text-base font-medium text-center text-white rounded-lg bg-white hover:bg-blue-400 hover:text-white focus:ring-4 focus:ring-blue-200">
                   Read more
                   <svg class="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                       <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
