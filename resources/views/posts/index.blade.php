1f24da012390eb93897ad95846028f3b09407c99
joaccco <joaccofranc02@icloud.com>
2023-10-31 10:26:21 -0300

    Cambios Esteticos vistas

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,9 +1,9 @@
 <x-app-layout>
-<section class="bg-white bg-[url(https://images.unsplash.com/photo-1553530979-7ee52a2670c4?auto=format&fit=crop&q=80&w=3302&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)] backdrop-blur-2xl">
+<section class="bg-white bg-[url(https://images.unsplash.com/photo-1501004318641-b39e6451bec6?auto=format&fit=crop&q=80&w=3346&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)] bg-center backdrop-blur-2xl">
 
     <div class="backdrop-blur-2xl w-screen">
         <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 ">
-            <h1 class="text-white text-3xl flex justify-center font-extrabold mt-10 mb-10">Posts y Anuncios</h1>
+            <h1 class="text-black text-3xl flex justify-center font-extrabold mt-16 mb-10">Posts y Anuncios</h1>
             <div class="grid md:grid-cols-2 gap-8">
             @foreach ($posts as $post)
                 {{-- @dd($post->image) --}}
@@ -16,10 +16,10 @@
                 }
                 @endphp
                 @if($post->image)
-                    <article style="background-image: url('{{ url(Storage::url($post->image->url)) }}'); background-position: center; " class="bg-black/20 border border-gray-200 rounded-lg p-8 md:p-12 @if($loop->first) col-span-2 mb-8 @endif">
+                    <article style="background-image: url('{{ url(Storage::url($post->image->url)) }}'); background-position: center; " class="bg-black/20 rounded-lg p-8 md:p-12 @if($loop->first) col-span-2 mb-8 @endif">
                         
                         @else
-                        <article class="bg-[url(https://images.unsplash.com/photo-1552832230-c0197dd311b5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1592&q=80)] bg-black/20 border border-gray-200 rounded-lg p-8 md:p-12 @if($loop->first) col-span-2 mb-8 @endif">
+                        <article class="bg-[url(https://images.unsplash.com/photo-1552832230-c0197dd311b5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1592&q=80)] bg-black/20 p-8 md:p-12 @if($loop->first) col-span-2 mb-8 @endif">
                         @endif
                         <a href="#" class="bg-white text-black text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded-md mb-2">
                             <svg class="w-2.5 h-2.5 mr-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 18">
